0ce8f52e44e717f7c1c6431856d3742d32b86edc
SomeSexyCake <jamisen8@gmail.com>
2015-12-18 16:03:39 -0500

    Update Command_al.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_al.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_al.java
@@ -17,31 +17,30 @@ public class Command_al extends TFM_Command
     {
         TFM_Util.playerMsg(sender_p, "======STAFF LIST======", ChatColor.GOLD);   
         TFM_Util.playerMsg(sender_p, "Founders", ChatColor.DARK_RED);
-        TFM_Util.playerMsg(sender_p, "-§4§k!§cSanikIsFast§4§k!", ChatColor.GRAY);
-        TFM_Util.playerMsg(sender_p, "- iHess", ChatColor.GRAY);
-        TFM_Util.playerMsg(sender_p, "- Secret_Pattern", ChatColor.GRAY);
-        TFM_Util.playerMsg(sender_p, "- heroguy42", ChatColor.GRAY);
+        TFM_Util.playerMsg(sender_p, "- §4§k!§cSomeSexyCake§4§k!", ChatColor.GRAY);
+        TFM_Util.playerMsg(sender_p, "- §4§k!§cSafoya§4§k!", ChatColor.GRAY);
         TFM_Util.playerMsg(sender_p, "Co-Founders", ChatColor.BLUE);
-        TFM_Util.playerMsg(sender_p, "- PlowableThree82", ChatColor.GRAY);
-        TFM_Util.playerMsg(sender_p, "- Ale8a", ChatColor.GRAY);
+        TFM_Util.playerMsg(sender_p, "- RemiXedFuzZion", ChatColor.GRAY);
         TFM_Util.playerMsg(sender_p, "Lead Developers", ChatColor.DARK_PURPLE);
         TFM_Util.playerMsg(sender_p, "- NONE", ChatColor.DARK_RED);
         TFM_Util.playerMsg(sender_p, "Developers", ChatColor.DARK_PURPLE);
-        TFM_Util.playerMsg(sender_p, "- XGreenPlayz12", ChatColor.GRAY);
+        TFM_Util.playerMsg(sender_p, "- NONE", ChatColor.GRAY);
         TFM_Util.playerMsg(sender_p, "Admin Manager", ChatColor.RED);
         TFM_Util.playerMsg(sender_p, "- NONE", ChatColor.GRAY);
+        TFM_Util.playerMsg(sender_p, "Staff Manager", ChatColor.RED);
+        TFM_Util.playerMsg(sender_p, "- SanikIsFast", ChatColor.GRAY);
         TFM_Util.playerMsg(sender_p, "System Admin", ChatColor.DARK_RED);
-        TFM_Util.playerMsg(sender_p, "- NONE", ChatColor.GRAY);
+        TFM_Util.playerMsg(sender_p, "- 360Redstone", ChatColor.GRAY);
         TFM_Util.playerMsg(sender_p, "Lead Specialists", ChatColor.GREEN);
-        TFM_Util.playerMsg(sender_p, "- Colin_Rocks", ChatColor.GRAY);
+        TFM_Util.playerMsg(sender_p, "- NONE", ChatColor.GRAY);
         TFM_Util.playerMsg(sender_p, "Specialists", ChatColor.GREEN);
-        TFM_Util.playerMsg(sender_p, "- NONE", ChatColor.DARK_BLUE);
+        TFM_Util.playerMsg(sender_p, "- Fence", ChatColor.DARK_BLUE);
         TFM_Util.playerMsg(sender_p, "Lead Executives", ChatColor.GOLD);
-        TFM_Util.playerMsg(sender_p, "- Parkerfd9dy", ChatColor.GRAY);
+        TFM_Util.playerMsg(sender_p, "- NONE", ChatColor.GRAY);
         TFM_Util.playerMsg(sender_p, "Executives", ChatColor.GOLD);
         TFM_Util.playerMsg(sender_p, "- NONE", ChatColor.DARK_BLUE);
         TFM_Util.playerMsg(sender_p, "Head Admin", ChatColor.YELLOW);
-        TFM_Util.playerMsg(sender_p, "- GoldenOscar", ChatColor.GRAY);
+        TFM_Util.playerMsg(sender_p, "- NONE", ChatColor.GRAY);
         TFM_Util.playerMsg(sender_p, "Senior Admin", ChatColor.LIGHT_PURPLE);
         sender_p.sendMessage(ChatColor.GRAY + StringUtils.join(TFM_AdminList.getSeniorAdminNames(), ", "));
         TFM_Util.playerMsg(sender_p, "Telnet Admin", ChatColor.DARK_GREEN);
@@ -50,7 +49,7 @@ public class Command_al extends TFM_Command
         sender_p.sendMessage(ChatColor.GRAY + StringUtils.join(TFM_AdminList.getSuperAdminNames(), ", "));
         TFM_Util.playerMsg(sender_p, "======STAFF LIST======", ChatColor.GOLD); 
         sender_p.sendMessage(ChatColor.RED + "If you're not in this list, be sure to tell us on the forums!");
-        sender_p.sendMessage(ChatColor.RED + "Remember that Developers will update this command Once week! Make sure you tell on the forums to get added and not left behind!");
+        sender_p.sendMessage(ChatColor.RED + "Remember that Developers will update this command once a week! Make sure you tell on the forums to get added and not get left behind!");
         
         return true;
     }
