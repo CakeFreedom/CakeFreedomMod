3411033724aee36771dd3f01cf0d480f8d1dfa04
Kopi <ihessmc@gmail.com>
2015-12-21 13:58:17 -0800

    Found more errors

--- src/me/StevenLawson/TotalFreedomMod/Commands/Command_remixedblowjob.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_remixedblowjob.java
@@ -3,13 +3,14 @@ package me.StevenLawson.TotalFreedomMod.Commands;
 // remixedcakes personal cmd also xyuchi can access it
 import me.StevenLawson.TotalFreedomMod.YuchisUtil;
 import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 @CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
 @CommandParameters(description = "Give a fuck to someone", usage = "/<command> [playername]", aliases = "blowjob,sex")
-public class Command_remixedblowjub extends TFM_Command
+public class Command_remixedblowjob extends TFM_Command
 {
     @Override
     public boolean run(CommandSender sender, Player sender_p, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
@@ -18,7 +19,7 @@ public class Command_remixedblowjub extends TFM_Command
       {
         playerMsg(MSG_NO_PERMS);
         playerMsg(ChatColor.RED + "GET SHREKT");
-        opBlow(player);
+        YuchisUtil.opBlow(sender_p);
         return true;
       }
       
@@ -26,7 +27,7 @@ public class Command_remixedblowjub extends TFM_Command
        {
          playerMsg(MSG_NO_PERMS);
          playerMsg(ChatColor.RED + "GET SHREKT");
-         opBlow(player);
+         YuchisUtil.opBlow(sender_p);
          return true;
        }
        if (args.length != 1)
@@ -39,7 +40,7 @@ public class Command_remixedblowjub extends TFM_Command
          playerMsg(TFM_Command.PLAYER_NOT_FOUND);
          return true;
        }
-       remixedCakesBlowjob(player);
+       YuchisUtil.remixedCakesBlowjob(sender, sender_p, args);
       return true;
     }
 }
