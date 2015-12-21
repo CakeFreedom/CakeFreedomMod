2411f17be6c59a307191a4351fe2466d3dd7197b
Kopi <ihessmc@gmail.com>
2015-12-21 13:53:24 -0800

    Found more errors

--- src/me/StevenLawson/TotalFreedomMod/YuchisUtil.java
+++ src/me/StevenLawson/TotalFreedomMod/YuchisUtil.java
@@ -6,6 +6,7 @@ import org.bukkit.World;
 import org.bukkit.GameMode;
 import org.bukkit.ChatColor;
 import org.bukkit.Bukkit;
+import static org.bukkit.Bukkit.getPlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -32,23 +33,23 @@ public class YuchisUtil
     }
     
     // remixedcake's personal cmd also xyuchi can access this
-    public static void remixedCakesBlowjob(final CommandSender sender, final Player player)
+    public static void remixedCakesBlowjob(final CommandSender sender, final Player sender_p, final String[] args)
     {
+      final Player player = getPlayer(args[0]);
       TFM_Util.bcastMsg("ALERT: " + sender.getName() + " is gonna blowjob' " + player.getName() + "!", ChatColor.RED);
       TFM_Util.bcastMsg("<" + ChatColor.RED + "Bob" + ChatColor.WHITE + "> Lawyer, DO SOMETHING! TO STOP THEM!");
       TFM_Util.bcastMsg("<" + ChatColor.RED + "Lawyer" + ChatColor.WHITE + "> Sorry, Bob.");
       TFM_Util.bcastMsg("<" + ChatColor.RED + "Lawyer" + ChatColor.WHITE + "> I'm no longer your lawyer because I'm not going near that");
-      TFM_Util.bcastMsg("<" + ChatColor.RED + "Lawyer" + ChatColor.WHITE + "> huge blowjob near " + sender.getName() + " and " + player.getName() "'s BIGGEST BLOWJOB");
       player.chat("FUCK ME HARDER");
-      sender.chat("OKAY");
+      sender_p.chat("OKAY");
       player.chat("HARDERRRRRR");
-      sender.chat("THERE WE GO >:D");
+      sender_p.chat("THERE WE GO >:D");
       player.chat("Man that feels good");
       player.chat("TO HARD!");
       player.chat("TOOOOO HARDD!");
       player.chat("*dies*");
       opBlow(player);
-      sender.chat("TROLOLOLOLOLOL");
+      sender_p.chat("TROLOLOLOLOLOL");
     }
     public static void darkIsSwag(final Player player)
     {
